9b979a831b63d67fb07094a23f1ab7496ac6804c
Prince73992 <150899396+Prince73992@users.noreply.github.com>
2024-02-13 13:10:37 +0530

    Add files via upload
    index file done

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="stat.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+    <div class="nav">
+    <div id="s1"><span> Student Management</span></div>
+    <div class="tea">
+    <img src="tea.png" class="img" alt="">
+    <div id="s2"><span> Nirmal Soyza</span></div>
+    </div>
+    </div>
+    <div class="main">
+        <div class="parent" id="dashboard">
+            <div class="box3"><a href="#" class="a1">Dashboard</a></div>
+            <div class="box4"><a href="statistics.php" class="a"><i class="fa fa-bar-chart" style="font-size:1vw"></i> Statistics</a></div>
+            <div class="box3"><a href="tchr.php" class="a"><i class="fa fa-book" style="font-size:1vw"></i> Teachers</a></div>
+            <div class="box3"><a href="parent.php" class="a"><i class="fa fa-group" style="font-size:1vw"></i>
+                Parents</a></div>
+            <div class="box3"><a href="student.php" class="a"><i class="fa fa-graduation-cap" style="font-size:1vw"></i>  Students</a></div>
+            <div class="box3"><a href="subject.php" class="a"><i class="fa fa-child" style="font-size:1vw"></i> Subjects</a></div>
+        </div>
+        <div class="parent" id="detail">
+        <span id="s3">School</span>
+        <span id="s4">overview</span>
+            <div class="total">
+                <div class="box5" id="b1">
+                    <span class="ttl"> Students <i class="fa fa-graduation-cap" ></i></span>
+                    <div class="box6">
+                        <a href="student.html" class="a"><i class="fa fa-graduation-cap" style="font-size:1.8vw"></i></a>
+                    </div>
+                </div>
+                <div class="box5" id="b2">
+                    <span class="ttl"> Teachers <i class="fa fa-book" ></i></span>
+                    <div class="box6">
+                        <a href="tchr.php" class="a"><i class="fa fa-book" style="font-size:1.8vw"></i></a>
+                    </div>
+                </div>
+                <div class="box5" id="b3">
+                    <span class="ttl"> Parent <i class="fa fa-group" ></i></span>
+                    <div class="box6">
+                        <a href="subject.php" class="a"><i class="fa fa-group" style="font-size:1.8vw"></i></a>
+                    </div>
+                </div>
+                <div class="box5" id="b4">
+                    <span class="ttl"> Students <i class="fa fa-graduation-cap" ></i></span>
+                    <div class="box6">
+                        <a href="student.html" class="a"><i class="fa fa-graduation-cap" style="font-size:1.8vw"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="parent.js"></script>
+</body>
+</html>
